a5576b40dbbb7ac8ce7b776414cce13736f90e97
Hundraw <chauvel.dimitri@posteo.net>
2022-11-14 11:54:25 +0100

    Configure Vitest

--- vite.config.js
+++ vite.config.js
@@ -23,5 +23,8 @@ export default defineConfig({
     minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG
+  },
+  test: {
+    include: ['test/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
   }
 })
